5c595c1a6bb982ca6bbd6fd81a6fdb8481913ae8
John Pugh <jpugh@suse.com>
2021-08-05 15:35:41 -0400

    Finish massive up

--- rkeinst/04-rancher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# install rancher
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-helm repo update
-kubectl create ns cattle-system
-helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=ec2-3-19-246-146.us-east-2.compute.amazonaws.com --set replicas=1
-
